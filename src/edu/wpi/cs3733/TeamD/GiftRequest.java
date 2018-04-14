dd64a51e743a4950e36f75d0c3702fb2d296425c
DBall8 <dcball@wpi.edu>
2018-04-14 01:09:55 -0400

    Set up database insertions and the basis of a lot of the classes as far as I can see them

--- src/edu/wpi/cs3733/TeamD/GiftRequest.java
+++ src/edu/wpi/cs3733/TeamD/GiftRequest.java
@@ -19,12 +19,12 @@ public class GiftRequest {
         try{
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GiftRequestScreen.fxml"));
-            stage.setTitle("edu.wpi.cs3733.TeamD.Entitiescs3733.TeamD.Gift Request");
+            stage.setTitle("Gift Request");
             stage.setScene(new Scene(root, windowWidth, windowLength));
             stage.show();
         }
         catch(IOException e){
-            System.out.println("Could not load edu.wpi.cs3733.TeamD.Gift3.TeamD.Entities.Gift Request screen.");
+            System.out.println("Could not load Gift Request screen.");
             e.printStackTrace();
             throw new ServiceException("Couldnt load main screen.");
         }
