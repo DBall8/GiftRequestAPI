772ef3480d40fbb01b8c7175be6bbb6d66bd5748
tdweiler <tdweiler>
2018-04-15 16:01:13 -0400

    Connected FXML with Controller, being worked on now

--- src/edu/wpi/cs3733/TeamD/FXMLControllers/GiftServiceRequestController.java
+++ src/edu/wpi/cs3733/TeamD/FXMLControllers/GiftServiceRequestController.java
@@ -1,19 +1,12 @@
 package edu.wpi.cs3733.TeamD.FXMLControllers;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTreeTableView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class GiftServiceRequestController extends ScreenController{
 
     @FXML
-    JFXButton requestGiftButton;
-
-    @FXML
-    private void buttonAction(ActionEvent e){
-        if(e.getSource() == requestGiftButton){
-
-        }
-    }
-
+    private JFXTreeTableView unassignedGifts;
 }
